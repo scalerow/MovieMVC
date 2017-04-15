50489b9038bd0b39345a9d81f9b72dd1be4de4ea
Fredrik Sundblom <jf.sundblom@outlook.com>
2017-04-15 19:24:55 +0200

    Added Models/Movies
    Added Modelels/Class.cs
    Added moviescontroller - scaffolding created "CRUD"

---
+++ Models/Class.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string genre { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
 
 namespace MvcMovie
 {
@@ -29,6 +31,9 @@ namespace MvcMovie
         {
             // Add framework services.
             services.AddMvc();
+
+            services.AddDbContext<MvcMovieContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("MvcMovieContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
